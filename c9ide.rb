6c12a6d66ea41896dc243ae4b30f311369eadad0
Alexandr Priezzhev <a@priezz.net>
2016-06-09 14:51:38 +0300

    c9ide 1.0.1

--- c9ide.rb
+++ c9ide.rb
@@ -1,9 +1,9 @@
 class C9ide < Formula
   desc "Cloud9 (c9.io) based local IDE"
   homepage "https://gitlab.com/priezz/c9ide"
-  url "https://gitlab.com/priezz/c9ide/repository/archive.tar.gz?ref=1.0.0"
-  version "1.0.0"
-  sha256 "63d65e9c10c217737dce2d2f1284c415ea95e729a1a0932204bc4dd8c8817769"
+  url "https://gitlab.com/priezz/c9ide/repository/archive.tar.gz?ref=1.0.1"
+  version "1.0.1"
+  sha256 "f0ef489bbd25a99ed812efade127fb56d88c53adfaa56ab003377a2c1f9217b7"
 
   # depends_on "cmake" => :build
   #depends_on :x11 # if your formula requires any X11/XQuartz components
